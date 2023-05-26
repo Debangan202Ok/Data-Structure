1bb4d0eea7348695192ff2281a155b9766c8f3be
paul <98002712+pauldebangan1970@users.noreply.github.com>
2023-05-26 14:44:10 +0530

    images are loading directly objet method failed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Pts, Images, Btn } from '/pagedata.js'
+import { Pts,Btn } from '/pagedata.js'
 import Body from '../components/Body.jsx'
 
 
@@ -8,9 +8,9 @@ export default function Home() {
     }
     return (
         <div>
-            <Body links={'labs/'} headQ={Pts.ht1} datas={Pts.pt1} btnC="bg-[#ff6633]" Imgs={Images.im1} Btns1={Btn.btn1} Btns2={Btn.btn2} btnPos="justify-between"/>
-            <Body id="sec2" pgLink={wikiClick} headQ={Pts.ht2} datas={Pts.pt2} btnC="bg-[#F478e6]" Imgs={Images.im2} Btns1={Btn.btn3} btn={Btn} pos1="order-2" pos2="order-1" btnPos="justify-end" visibilty2="hidden" />
-            <Body headQ={Pts.ht3} datas={Pts.pt3} Imgs={Images.im3} btnPos="hidden" />
+            <Body links={'labs/'} headQ={Pts.ht1} datas={Pts.pt1} btnC="bg-[#ff6633]" Imgs={'/images/R.png'} Btns1={Btn.btn1} Btns2={Btn.btn2} btnPos="justify-between"/>
+            <Body id="sec2" pgLink={wikiClick} headQ={Pts.ht2} datas={Pts.pt2} btnC="bg-[#F478e6]" Imgs={'/images/data.png'} Btns1={Btn.btn3} btn={Btn} pos1="order-2" pos2="order-1" btnPos="justify-end" visibilty2="hidden" />
+            <Body headQ={Pts.ht3} datas={Pts.pt3} Imgs={'/images/data1.png'} btnPos="hidden" />
         </div>
     )
 }
