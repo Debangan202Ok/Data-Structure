6f1a6ee49b0e0644def9d24b9c955f2b5d04dc3f
paul <98002712+pauldebangan1970@users.noreply.github.com>
2023-05-26 14:50:33 +0530

    images are loading directly objet method failed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     }
     return (
         <div>
-            <Body links={'labs/'} headQ={Pts.ht1} datas={Pts.pt1} btnC="bg-[#ff6633]" Imgs={'/images/R.png'} Btns1={Btn.btn1} Btns2={Btn.btn2} btnPos="justify-between"/>
+            <Body links={'labs/'} headQ={Pts.ht1} datas={Pts.pt1} btnC="bg-[#ff6633]" Imgs={'../images/data.png'} Btns1={Btn.btn1} Btns2={Btn.btn2} btnPos="justify-between"/>
             <Body id="sec2" pgLink={wikiClick} headQ={Pts.ht2} datas={Pts.pt2} btnC="bg-[#F478e6]" Imgs={'/images/data.png'} Btns1={Btn.btn3} btn={Btn} pos1="order-2" pos2="order-1" btnPos="justify-end" visibilty2="hidden" />
             <Body headQ={Pts.ht3} datas={Pts.pt3} Imgs={'/images/data1.png'} btnPos="hidden" />
         </div>
