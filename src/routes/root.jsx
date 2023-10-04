b15c516bfae6ecc2e9df63cd6f7be96cd7e2fba7
Toyin Muhammed <toyinmuhammed50@gmail.com>
2023-10-04 08:42:12 +0200

    feat: added footer component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar.jsx'
+import Footer from '../components/Footer'
 
 export default function Root() {
     return (
         <div>
             <Navbar />
             <Outlet />
+	    <Footer />
         </div>
     )
 }
