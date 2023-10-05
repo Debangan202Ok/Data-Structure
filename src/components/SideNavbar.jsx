0b43ac604f8b8c1fd0360136fc9406e649dee6f6
Miran <miranfirdausi027@gmail.com>
2023-10-05 23:36:42 +0530

    aligned the icon

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -10,7 +10,7 @@ export default function DefaultSidebar() {
     <div className="fixed z-20">
       <div
         id="sidebarToggle"
-        className="fixed md:hidden p-2 bg-gray-700 text-white cursor-pointer rounded-lg m-3 z-50"
+        className="fixed md:hidden p-2 bg-gray-700 text-white cursor-pointer rounded-lg m-3 ml-[calc(5%+10px)] z-50"
         onClick={toggleSidebar}
       >
         <button
@@ -23,9 +23,9 @@ export default function DefaultSidebar() {
         </button>
       </div>
       <Card className="side-bar translate-x-[-100%] md:translate-x-[0%] md:block h-[calc(100vh-48px)] rounded-none w-[18rem] p-4 shadow-xl bg-[#f7f7f7] shadow-blue-gray-900/5">
-        <div className="ml-[50px] mb-[10px] p-2 md:ml-[0]">
+        <div className="ml-[calc(5vw+56px)] mb-[10px] p-2 md:ml-[0]">
           <Typography variant="h5" color="blue-gray">
-            Data Structures Labs
+            Data Structures
           </Typography>
         </div>
         <List>
