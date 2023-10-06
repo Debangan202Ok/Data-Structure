ba389d5214c9198c56178d27606b494d52b5d502
Dav.O <davolufuwa@gmail.com>
2023-10-06 10:55:12 +0100

    Enhancement: updated  Footer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <footer class="bg-gray-900 text-white px-4 py-8">
-      <div class="flex flex-col justify-between md:flex-row">
+      <div class="flex flex-col justify-between gap-8 md:flex-row md:pt-3 sm:px-4 lg:px-12 lg:justify-start lg:gap-44">
         <div class="mb-4 md:mb-0">
-          <h1 class="text-3xl font-bold mt-4">DataStructure</h1>
+          <h1 class="text-3xl font-bold mt-4 md:mt-0">DataStructure</h1>
           <p class="text-gray-400 mt-2">
             A React app to learn data structures and algorithms.
           </p>
         </div>
-        <div class="flex flex-row flex-wrap justify-between gap-3">
+        <div class="flex flex-row flex-wrap gap-12 mt-2 md:mt-0 lg:gap-20">
           <div class="mb-4 md:mb-0">
             <h2 class="text-lg font-bold">Links</h2>
-            <ul class="mt-2">
+            <ul class="mt-2 text-sm flex flex-col gap-4">
               <li>
                 <a href="/" class="text-gray-400 hover:text-white">
                   Home
@@ -29,11 +29,16 @@ const Footer = () => {
                   Contact
                 </a>
               </li>
+              <li>
+                <a href="/contact" class="text-gray-400 hover:text-white">
+                  Privacy Policy
+                </a>
+              </li>
             </ul>
           </div>
           <div class="mb-4 md:mb-0 pr-2">
             <h2 class="text-lg font-bold">Social</h2>
-            <ul class="mt-2">
+            <ul class="mt-2 text-sm flex flex-col gap-4">
               <li>
                 <a
                   href="https://twitter.com/datastructureapp"
@@ -62,16 +67,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div class="flex flex-row justify-between mt-8">
-        <p class="text-gray-400">
+      <div class="text-center mt-8 mb-2 lg:mt-16">
+        <p class="text-gray-400 text-xs">
           Copyright © 2023 DataStructure. All rights reserved.
         </p>
-        <a
-          href="https://privacy.policy/"
-          class="text-gray-400 hover:text-white"
-        >
-          Privacy Policy
-        </a>
       </div>
     </footer>
   );
